1876be900285799e4b4f43f5c980159c050a23b9
Ben Metzger <benjpac@gmail.com>
2017-07-01 18:51:20 -0700

    seeded db using faker

--- Gemfile
+++ Gemfile
@@ -27,7 +27,6 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'faker'
 gem 'bootstrap-sass', '~> 3.3.6'
-gem 'sass-rails', '>= 3.2'
 gem 'jquery-rails'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+Review.destroy_all
+
+50.times do |index|
+  newProduct = Product.create!(name: Faker::Food.ingredient,
+                               cost: Faker::Number.between(25, 2000),
+                               country: Faker::GameOfThrones.city)
+  10.times do |index|
+    Review.create!(author: Faker::LordOfTheRings.character,
+                   content_body: Faker::Hipster.paragraph(4, true, 4),
+                   rating: Faker::Number.between(1, 100),
+                   product_id: newProduct.id)
+  end
+end
+
+p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
