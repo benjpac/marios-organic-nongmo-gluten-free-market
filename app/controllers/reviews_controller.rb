9b3d892c97e8e6e13021829ed5b38895dc1494dc
Ben Metzger <benjpac@gmail.com>
2017-07-07 19:06:49 -0700

    add functionality to render partial review within product page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,50 +1,24 @@
 class ReviewsController < ApplicationController
-  # before_action :find_product
 
-  # def new
-  #   @review = @product.reviews.new
-  # end
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+  end
 
-  # def create
-  #   @review = @product.reviews.new(review_params)
-  #   if @review.save
-  #     redirect_to product_path(@product), notice: 'Review added'
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render :_new
+    end
+  end
 
-  # def edit
-  #   find_review
-  # end
+  private
 
-  # def update
-  #   find_review
-  #   if @review.update 
-  #     redirect_to product_path(@product), notice: "Review updated"
-  #   else
-  #     render :edit
-  #   end
-  # end
-
-  # def destroy
-  #   find_review
-  #   @review.destroy
-  #   redirect_to product_path(@product), notice: 'Review deleted'
-  # end
-
-# private
-
-#   def review_params
-#     params.require(:review).permit(:author, :content_body, :rating)
-#   end
-
-#   def find_product
-#     @product = Product.find(params[:product_id])
-#   end
-
-  # def find_review
-  #   @review = Review.find(params[:id])
-  # end
+  def review_params
+    params.require(:review).permit(:author, :content_body, :rating, :product_id)
+  end
 
 end
